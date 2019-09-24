b0903e8e30e25e0fb1cbf7e4b0fb784b6f4f0f87
ktan114 <ktaneight@gmail.com>
2019-09-24 19:49:08 -0400

    Modify the saveTodo to display and create TodoList

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Typography from '@material-ui/core/Typography';
 
 import './App.css';
 import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
       <Typography component="h1" variant="h2">
         Todos
       </Typography>
-      <TodoForm saveTodo={console.warn} />
+      <TodoForm
+        saveTodo={todoText => {
+          const trimmedText = todoText.trim();
+          if (trimmedText.length > 0) {
+            setTodos([...todos, trimmedText]);
+          }
+        }}
+      />
+      <TodoList todos={todos} />
     </div>
   );
 }
