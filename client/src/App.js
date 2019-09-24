4565be4f9676d3890e9058767c3900fccb213fb9
ktan114 <ktaneight@gmail.com>
2019-09-24 19:36:03 -0400

    Starter code

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Typography component="h1" variant="h2">
+        Todos
+      </Typography>
+    </div>
+  );
+}
+
+export default App;
