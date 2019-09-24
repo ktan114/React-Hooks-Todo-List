075a61e20494dd5ad86bcd53e9c1178cb9312179
ktan114 <ktaneight@gmail.com>
2019-09-24 19:55:03 -0400

    Abstract the state from the App component

--- client/src/App.js
+++ client/src/App.js
@@ -4,32 +4,54 @@ import Typography from '@material-ui/core/Typography';
 import './App.css';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
+import useTodoState from './container/useTodoState';
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const { todos, addTodo, deleteTodo } = useTodoState([]);
+
   return (
     <div className="App">
       <Typography component="h1" variant="h2">
         Todos
       </Typography>
+
       <TodoForm
         saveTodo={todoText => {
           const trimmedText = todoText.trim();
+
           if (trimmedText.length > 0) {
-            setTodos([...todos, trimmedText]);
+            addTodo(trimmedText);
           }
         }}
       />
-      <TodoList
-        todos={todos}
-        deleteTodo={todoIndex => {
-          const newTodos = todos.filter((_, index) => index !== todoIndex);
 
-          setTodos(newTodos);
-        }}
-      />
+      <TodoList todos={todos} deleteTodo={deleteTodo} />
     </div>
   );
+  // const [todos, setTodos] = useState([]);
+  // return (
+  //   <div className="App">
+  //     <Typography component="h1" variant="h2">
+  //       Todos
+  //     </Typography>
+  //     <TodoForm
+  //       saveTodo={todoText => {
+  //         const trimmedText = todoText.trim();
+  //         if (trimmedText.length > 0) {
+  //           setTodos([...todos, trimmedText]);
+  //         }
+  //       }}
+  //     />
+  //     <TodoList
+  //       todos={todos}
+  //       deleteTodo={todoIndex => {
+  //         const newTodos = todos.filter((_, index) => index !== todoIndex);
+
+  //         setTodos(newTodos);
+  //       }}
+  //     />
+  //   </div>
+  // );
 }
 
 export default App;
